3b88c11d3223987849632c3b7c197e5f95e8d5a9
arijit_charbi <arijitcharbi2001@gmail.com>
2021-10-31 19:39:56 +0530

    Modified the admin panel

--- CSA/views.py
+++ CSA/views.py
@@ -7,19 +7,19 @@ def admission(request):
     context = {
         'data' : models.Quote.objects.all(),
     }
-    return render(request, ('csa/csa.html'), context=context)
+    return render(request, ('csa/csa.html'), context = context)
 def msc(request):
     context = {
         'data' : models.QuoteMSC.objects.all(),
     }
-    return render(request, ('csa/msc.html'), context=context)
+    return render(request, ('csa/msc.html'), context = context)
 def bsc(request):
     context = {
         'data' : models.QuoteBSC.objects.all(),
     }
-    return render(request, ('csa/bsc.html'), context=context)
+    return render(request, ('csa/bsc.html'), context = context)
 def bca(request):
     context = {
         'data' : models.QuoteBCA.objects.all(),
     }
-    return render(request, ('csa/bca.html'), context=context)
+    return render(request, ('csa/bca.html'), context = context)
